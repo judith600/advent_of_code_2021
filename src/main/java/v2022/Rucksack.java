2394af428c2966d8e3d6bfa84434909e4d581b87
Judith Hurzlmeier <Judith.Hurzlmeier@cas.de>
2022-12-08 12:18:13 +0100

    2022 Day 3 Part 2

--- src/main/java/v2022/Rucksack.java
+++ src/main/java/v2022/Rucksack.java
@@ -1,9 +1,12 @@
 package v2022;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+
+import com.google.common.collect.Lists;
 
 import common.TwoParts;
 
@@ -16,7 +19,8 @@ public class Rucksack implements TwoParts {
 
 	@Override
 	public int getPartTwo(List<String> input) {
-		return 0;
+		List<List<String>> groups = Lists.partition(new ArrayList<>(input), 3);
+		return groups.stream().map(this::getSharedGroupItem).map(this::getPriority).reduce(Integer::sum).orElse(-1);
 	}
 
 	public String getSharedItem(String rucksack) {
@@ -29,6 +33,20 @@ public class Rucksack implements TwoParts {
 		Set<Character> secondSet = secondCompartment.chars().mapToObj(e -> (char) e).collect(Collectors.toSet());
 
 		List<String> sharedItem = firstSet.stream().filter(secondSet::contains).map(String::valueOf).toList();
+		return sharedItem.get(0);
+	}
+
+	public String getSharedGroupItem(List<String> group) {
+		List<Set<Character>> sets = group.stream()
+				.map(elem -> elem.chars().mapToObj(e -> (char) e).collect(Collectors.toSet()))
+				.toList();
+
+		List<String> sharedItem = sets.get(0)
+				.stream()
+				.filter(sets.get(1)::contains)
+				.filter(sets.get(2)::contains)
+				.map(String::valueOf)
+				.toList();
 		return sharedItem.get(0);
 	}
 
--- src/test/java/v2022/RucksackTest.java
+++ src/test/java/v2022/RucksackTest.java
@@ -3,6 +3,7 @@ package v2022;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
+import java.util.Objects;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -37,6 +38,19 @@ class RucksackTest {
 
 		assertEquals(157, rucksack.getPartOne(testInput));
 
-		System.out.println(rucksack.getPartOne(ReadInputUtil.readLines("rucksack")));
+		System.out.println(rucksack.getPartOne(Objects.requireNonNull(ReadInputUtil.readLines("rucksack"))));
+	}
+
+	@Test
+	public void partTwo() {
+		assertEquals("r",
+				rucksack.getSharedGroupItem(
+						List.of("vJrwpWtwJgWrhcsFMMfFFhFp", "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL", "PmmdzqPrVvPwwTWBwg")));
+
+		assertEquals("Z",
+				rucksack.getSharedGroupItem(
+						List.of("wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn", "ttgJtRGJQctTZtZT", "CrZsJsPPZsGzwwsLwLmpwMDw")));
+
+		System.out.println(rucksack.getPartTwo(Objects.requireNonNull(ReadInputUtil.readLines("rucksack"))));
 	}
 }
